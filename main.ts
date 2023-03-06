aeae463939c00d6c3e295fea81485a42853e7072
lmzgreig <127131845+lmzgreig@users.noreply.github.com>
2023-03-06 16:32:16 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    music.playTone(131, music.beat(BeatFraction.Half))
+    music.playTone(165, music.beat(BeatFraction.Half))
+    music.playTone(196, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(196, music.beat(BeatFraction.Half))
+    music.playTone(165, music.beat(BeatFraction.Half))
+    music.playTone(131, music.beat(BeatFraction.Half))
+})
